4ce012bf8fad9dd1dfa523b6575e0870387cfe5a
faizankhananics <faizan.khanani@centricsource.com>
2016-04-14 18:08:52 +0500

    Initial Commit

---
+++ iOSTrainingWithObjectiveC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSTrainingWithObjectiveC
+//
+//  Created by Faizan Saleem Khanani on 14/04/2016.
+//  Copyright © 2016 CS-LTF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
